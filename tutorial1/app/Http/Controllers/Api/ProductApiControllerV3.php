5231232e590fd182efc2e5068f12f57643fcef86
Manuela <mcastanof1@eafit.edu.co>
2025-03-27 20:17:55 -0500

    tutorial4

--- tutorial1/app/Http/Controllers/Api/ProductApiControllerV3.php
+++ tutorial1/app/Http/Controllers/Api/ProductApiControllerV3.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductCollection;
 use App\Models\Product;
@@ -11,12 +12,14 @@ class ProductApiControllerV3 extends Controller
     public function index(): JsonResponse
     {
         $products = new ProductCollection(Product::all());
+
         return response()->json($products, 200);
     }
 
     public function paginate(): JsonResponse
     {
         $products = new ProductCollection(Product::paginate(5));
+
         return response()->json($products, 200);
     }
 }
